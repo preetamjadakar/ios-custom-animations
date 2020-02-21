d872050fe8c40d62728bc6e2b9973692a4e74fd7
Preetam Jadakar <preetam.jadakar@nihilent.com>
2020-02-21 15:39:49 +0530

    fix: font correction

---
+++ Animations/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Animations
+//
+//  Created by admin on 21/02/20.
+//  Copyright © 2020 Nihilent. All rights reserved.
+//
+
+import Foundation
+
+//let fontStyle1 =
+//FONTNAME:RobotoMono-Regular
+//FONTNAME:RobotoMono-Light
+//FONTNAME:RobotoMono-Medium
